5cc8f6a5c2e693a7cb82b82cfae746f4949df1a4
Ezequiel Otero <ezeotero@gmail.com>
2020-09-26 19:13:04 -0300

    gallery completa

--- require/gallery.php
+++ require/gallery.php
@@ -10,64 +10,64 @@
 
           <div class="col-lg-3 col-md-4">
             <div class="gallery-item">
-              <a href="assets/img/gallery/gallery-1.jpg" class="venobox" data-gall="gallery-item">
-                <img src="assets/img/gallery/gallery-1.jpg" alt="" class="img-fluid">
+              <a href="assets/img/gallery/hamburguesa_1.jpg" class="venobox" data-gall="gallery-item">
+                <img src="assets/img/gallery/hamburguesa_1.jpg" alt="" class="img-fluid">
               </a>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-4">
             <div class="gallery-item">
-              <a href="assets/img/gallery/gallery-2.jpg" class="venobox" data-gall="gallery-item">
-                <img src="assets/img/gallery/gallery-2.jpg" alt="" class="img-fluid">
+              <a href="assets/img/gallery/hamburguesa_13.jpg" class="venobox" data-gall="gallery-item">
+                <img src="assets/img/gallery/hamburguesa_13.jpg" alt="" class="img-fluid">
               </a>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-4">
             <div class="gallery-item">
-              <a href="assets/img/gallery/gallery-3.jpg" class="venobox" data-gall="gallery-item">
-                <img src="assets/img/gallery/gallery-3.jpg" alt="" class="img-fluid">
+              <a href="assets/img/gallery/hamburguesa_10.jpg" class="venobox" data-gall="gallery-item">
+                <img src="assets/img/gallery/hamburguesa_10.jpg" alt="" class="img-fluid">
               </a>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-4">
             <div class="gallery-item">
-              <a href="assets/img/gallery/gallery-4.jpg" class="venobox" data-gall="gallery-item">
-                <img src="assets/img/gallery/gallery-4.jpg" alt="" class="img-fluid">
+              <a href="assets/img/gallery/hamburguesa_15.jpg" class="venobox" data-gall="gallery-item">
+                <img src="assets/img/gallery/hamburguesa_15.jpg" alt="" class="img-fluid">
               </a>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-4">
             <div class="gallery-item">
-              <a href="assets/img/gallery/gallery-5.jpg" class="venobox" data-gall="gallery-item">
-                <img src="assets/img/gallery/gallery-5.jpg" alt="" class="img-fluid">
+              <a href="assets/img/gallery/hamburguesa_5.jpg" class="venobox" data-gall="gallery-item">
+                <img src="assets/img/gallery/hamburguesa_5.jpg" alt="" class="img-fluid">
               </a>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-4">
             <div class="gallery-item">
-              <a href="assets/img/gallery/gallery-6.jpg" class="venobox" data-gall="gallery-item">
-                <img src="assets/img/gallery/gallery-6.jpg" alt="" class="img-fluid">
+              <a href="assets/img/gallery/hamburguesa_17.jpg" class="venobox" data-gall="gallery-item">
+                <img src="assets/img/gallery/hamburguesa_17.jpg" alt="" class="img-fluid">
               </a>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-4">
             <div class="gallery-item">
-              <a href="assets/img/gallery/gallery-7.jpg" class="venobox" data-gall="gallery-item">
-                <img src="assets/img/gallery/gallery-7.jpg" alt="" class="img-fluid">
+              <a href="assets/img/gallery/hamburguesa_16.jpg" class="venobox" data-gall="gallery-item">
+                <img src="assets/img/gallery/hamburguesa_16.jpg" alt="" class="img-fluid">
               </a>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-4">
             <div class="gallery-item">
-              <a href="assets/img/gallery/gallery-8.jpg" class="venobox" data-gall="gallery-item">
-                <img src="assets/img/gallery/gallery-8.jpg" alt="" class="img-fluid">
+              <a href="assets/img/gallery/hamburguesa_12.jpg" class="venobox" data-gall="gallery-item">
+                <img src="assets/img/gallery/hamburguesa_12.jpg" alt="" class="img-fluid">
               </a>
             </div>
           </div>
